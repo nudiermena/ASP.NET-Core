fcd7bfa760fc8f4a1fc39f08dbf20602f7e7bb4e
nudier.mena <nudier716@hotmail.com>
2017-07-18 18:06:03 -0500

    Agregar archivos de proyecto.

---
+++ WebAppDockerContainer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebAppDockerContainer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+#if IISEXPRESS
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()                
+                .Build();
+
+            host.Run();
+#else
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+
+#endif
+        }
+    }
+}
